10369ea3c69595edaf802881f5a0f512a973071e
jugalcodes <122133555+jugalcodes@users.noreply.github.com>
2024-10-20 23:53:14 +0530

    Update utils.js

--- frontend/src/api/utils.js
+++ frontend/src/api/utils.js
@@ -3,4 +3,4 @@ import {toast} from 'react-toastify'
 export const notify = (message, type) => {
     toast[type](message);
 }
-export const API_URL = "https://21-bcp-240-jugal-chhatriwala-todo-app.vercel.app";
+export const API_URL = "https://21-bcp-240-jugal-chhatri-git-7918a7-jugal-chhatriwalas-projects.vercel.app/?vercelToolbarCode=ZWomT5la7hiQsSY";
